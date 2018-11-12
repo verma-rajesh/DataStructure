e140d40de815e2540d3db16d17dd102dff8c9bde
Rajesh <verma-rajesh@outlook.com>
2018-11-12 23:50:41 +0530

    rotation of array by one in clockwise

---
+++ cyrot.cpp
@@ -0,0 +1,40 @@
+//Program for cyclically rotation by one
+void cyclicrotation(int arr[], int );
+#include<iostream>
+
+void cyclicrotation(int arr[],int n)
+{
+	int x=arr[n-1];
+
+	std::cout<<"function rotation:";
+
+	for (int i = n-1; i >0; --i)
+		std::wcout<< arr[i]=arr[i-1]<< "  ";
+		std::wcout<< arr[0]=x<< "  ";
+	
+
+}
+
+int main()
+{
+	int arr[5]={1,2,3,4,5},i;
+
+	int n=sizeof(arr)/sizeof(arr[0]);
+	
+	std::cout<<n;
+	
+	std::cout<<"array are:\n";
+	
+	for (int i = 0; i < n; ++i)
+	{
+		std::cout<< arr[i] <<"  ";
+	}
+
+	cyclicrotation(arr,n);
+
+	//std::cout << "\nRotated array is\n"; 
+    //for (i = 0; i < n; i++) 
+        //std::cout << arr[i]<<"  "; 
+  	
+	return 0;
+}
